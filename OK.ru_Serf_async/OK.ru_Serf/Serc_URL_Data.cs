c2123a28396ba733bb630492901fd02d5811754d
Kirill <36329442+Geabriel@users.noreply.github.com>
2021-03-23 15:06:41 +0300

    Add files via upload

---
+++ OK.ru_Serf_async/OK.ru_Serf/Serc_URL_Data.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OK.ru_Serf
+{
+    class Serc_URL_Data
+    {
+        private string url;
+        private string date;
+
+        public Serc_URL_Data(string url, string date)
+        {
+            this.Url = url;
+            this.Date = date;
+        }
+
+        public string Url
+        {
+            get
+            {
+                return url;
+            }
+
+            set
+            {
+                url = value;
+            }
+        }
+
+        public string Date
+        {
+            get
+            {
+                return date;
+            }
+
+            set
+            {
+                date = value;
+            }
+        }
+    }
+}
